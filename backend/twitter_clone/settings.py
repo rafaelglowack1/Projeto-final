e9bf15f52f1dc08f0b7f83ce80a50bf6b623e416
rafaelglowack1 <149637380+rafaelglowack1@users.noreply.github.com>
2025-11-12 09:30:58 -0300

    abrindo o projeto

--- backend/twitter_clone/settings.py
+++ backend/twitter_clone/settings.py
@@ -154,8 +154,6 @@ SIMPLE_JWT = {
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'diegocavalcantidev.pythonanywhere.com', 'twitter-sigma.vercel.app']
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
